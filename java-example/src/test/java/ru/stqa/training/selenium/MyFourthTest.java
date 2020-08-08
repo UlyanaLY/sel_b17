31d2663a152cebe16c799adb52b96518dc56072e
UlyanaLY <oulyana@gmail.com>
2020-08-08 23:46:24 +0300

    Задание 7. Фикс.

--- java-example/src/test/java/ru/stqa/training/selenium/MyFourthTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFourthTest.java
@@ -12,15 +12,14 @@ public class MyFourthTest extends TestBase {
     @Test
     public void verifyStickers() {
         driver.navigate().to("http://localhost/litecart/en/");
-        List<WebElement> items = driver.findElements(By.cssSelector(".image-wrapper"));
+        List<WebElement> items = driver.findElements(By.cssSelector(".product"));
 
         for (WebElement item : items) {
-            Assert.assertTrue(isElementPresent(item, By.cssSelector(".sticker")));
-            Assert.assertFalse(areElementsPresent(item, By.cssSelector(".sticker")));
+            Assert.assertTrue(hasOnlyOneElement(item, By.cssSelector(".sticker")));
         }
     }
 
-    boolean areElementsPresent(WebElement el, By locator) {
-        return el.findElements(locator).size() > 1;
+    boolean hasOnlyOneElement(WebElement el, By locator) {
+        return el.findElements(locator).size() == 1;
     }
 }
