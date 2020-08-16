a5c84c12bc7ca79f5b1f287a351cf2a9fa99e8a2
UlyanaLY <oulyana@gmail.com>
2020-08-16 15:44:24 +0300

    9 Задание. Проверка сортировки геозон

--- java-example/src/test/java/ru/stqa/training/selenium/MyFifthTest.java
+++ java-example/src/test/java/ru/stqa/training/selenium/MyFifthTest.java
@@ -8,12 +8,12 @@ import java.util.*;
 
 import static org.openqa.selenium.By.xpath;
 
-public class MyFifthTest extends TestBase{
+public class MyFifthTest extends TestBase {
     @Test
-    public void areCountriesAndZonesSorted(){
+    public void areCountriesAndZonesSorted() {
         driver.navigate().to("http://localhost/litecart/admin/?app=countries&doc=countries");
         List<WebElement> countries = driver.findElements(xpath("//tr[@class='row']//td[5]//a"));
-        int countriesSize =  countries.size();
+        int countriesSize = countries.size();
         ArrayList<String> countriesNameSorted = new ArrayList<>();
         ArrayList<String> countriesNameAnSorted = new ArrayList<>();
 
@@ -27,10 +27,10 @@ public class MyFifthTest extends TestBase{
 
         //Проверить, что зоны отсортированы
         for (int j = 1; j < countries.size(); j++) {
-            if (!driver.findElement(xpath("//tr[@class='row'][" + j + "]//td[6]")).getText().equals("0")){
+            if (!driver.findElement(xpath("//tr[@class='row'][" + j + "]//td[6]")).getText().equals("0")) {
                 driver.findElement(xpath("//tr[@class='row'][" + j + "]//td[7]")).click();
                 List<WebElement> zones = driver.findElements(xpath("//table[@class='dataTable']//tr//td[3]"));
-                int zoneSize =  zones.size() - 1;
+                int zoneSize = zones.size() - 1;
                 ArrayList<String> zonesNameSorted = new ArrayList<>();
                 ArrayList<String> zonesNameAnSorted = new ArrayList<>();
                 for (int k = 0; k < zoneSize; k++) {
@@ -41,6 +41,46 @@ public class MyFifthTest extends TestBase{
                 Assert.assertEquals(zonesNameSorted, zonesNameAnSorted);
 
                 driver.findElement(xpath(("//ul[@id='box-apps-menu']//li[3]"))).click();
+            }
+        }
+    }
+
+    @Test
+    public void areGeoZonesSorted() {
+        driver.navigate().to("http://localhost/litecart/admin/?app=geo_zones&doc=geo_zones");
+        List<WebElement> countries = driver.findElements(xpath("//table[@class='dataTable']//tr//td[3]"));
+        int countriesSize = countries.size();
+        ArrayList<String> countriesNameSorted = new ArrayList<>();
+        ArrayList<String> countriesNameAnSorted = new ArrayList<>();
+
+        //Проверить, что страны отсортированы
+        for (int i = 0; i < countriesSize; i++) {
+            countriesNameSorted.add(countries.get(i).getText());
+            countriesNameAnSorted.add(countries.get(i).getText());
+        }
+        System.out.println(countriesNameSorted);
+        System.out.println(countriesNameAnSorted);
+        Collections.sort(countriesNameSorted);
+        Assert.assertEquals(countriesNameSorted, countriesNameAnSorted);
+
+        //Проверить, что зоны отсортированы
+        for (int j = 1; j <= countries.size(); j++) {
+            if (!driver.findElement(xpath("//tr[@class='row'][" + j + "]//td[4]")).getText().equals("0")) {
+                driver.findElement(xpath("//tr[@class='row'][" + j + "]//td[5]")).click();
+                List<WebElement> zones = driver.findElements(xpath("//table[@class='dataTable']//tr//td[3]//select/option[@selected='selected']"));
+
+                int zoneSize = zones.size();
+
+                ArrayList<String> zonesNameSorted = new ArrayList<>();
+                ArrayList<String> zonesNameAnSorted = new ArrayList<>();
+                for (int k = 0; k < zoneSize; k++) {
+                    zonesNameSorted.add(zones.get(k).getText());
+                    zonesNameAnSorted.add(zones.get(k).getText());
+                }
+                Collections.sort(zonesNameSorted);
+                Assert.assertEquals(zonesNameSorted, zonesNameAnSorted);
+
+                driver.findElement(xpath(("//ul[@id='box-apps-menu']//li[6]"))).click();
             }
         }
     }
